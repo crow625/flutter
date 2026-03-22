8aa891edfd2aac123a987eaae75313621795a445
crow625 <ethanscrow@gmail.com>
2026-03-21 20:01:23 -0700

    restyle transaction row

--- lib/src/widgets/transaction_row.dart
+++ lib/src/widgets/transaction_row.dart
@@ -9,16 +9,43 @@ class TransactionRow extends StatelessWidget {
     return '\$${(transaction.amountCents / 100).toStringAsFixed(2)}';
   }
 
+  String _formatDate(DateTime date) {
+    return '${date.month.toString().padLeft(2, '0')}/${date.day.toString().padLeft(2, '0')}/${date.year}';
+  }
+
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return Row(
-      spacing: 4,
       children: [
-        Text(transaction.id.toString()),
-        Text(transaction.vendor),
-        Text(transaction.category),
-        Text(_formatAmount()),
-        Text(transaction.time.toIso8601String()),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                _formatDate(transaction.time),
+                style: textTheme.labelSmall?.copyWith(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+              ),
+              Text(
+                transaction.vendor,
+                style: textTheme.labelMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 90,
+          child: Text(
+            _formatAmount(),
+            textAlign: TextAlign.right,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
       ],
     );
   }
--- lib/src/widgets/transaction_table.dart
+++ lib/src/widgets/transaction_table.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widget_previews.dart';
 import 'package:flutter_app/src/data/transactions.dart';
 import 'package:flutter_app/src/transaction/transaction_model.dart';
+import 'package:flutter_app/src/widgets/transaction_row.dart';
 
 class TransactionTable extends StatelessWidget {
   final List<TransactionModel> transactions;
@@ -14,53 +15,10 @@ class TransactionTable extends StatelessWidget {
   TransactionTable.preview({super.key})
       : transactions = Transactions.transactions;
 
-  String _formatAmount(TransactionModel t) {
-    return '\$${(t.amountCents / 100).toStringAsFixed(2)}';
-  }
-
-  String _formatDate(DateTime date) {
-    return '${date.month}/${date.day}/${date.year}';
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Table(
-      columnWidths: const {
-        0: FixedColumnWidth(60),
-        1: FlexColumnWidth(),
-        2: FixedColumnWidth(90),
-      },
-      children: transactions
-          .map(
-            (t) => TableRow(
-              children: [
-                DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    border: BoxBorder.all(
-                      color: Colors.black,
-                    ),
-                  ),
-                  child: Text(
-                    _formatAmount(t),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                DecoratedBox(
-                  decoration: const BoxDecoration(
-                    border: BoxBorder.symmetric(
-                      vertical: BorderSide(),
-                    ),
-                  ),
-                  child: Text(t.vendor),
-                ),
-                Text(
-                  _formatDate(t.time),
-                ),
-              ],
-            ),
-          )
-          .toList(),
+    return Column(
+      children: transactions.map((t) => TransactionRow(t)).toList(),
     );
   }
 }
@@ -68,6 +26,10 @@ class TransactionTable extends StatelessWidget {
 PreviewThemeData previewThemeData() {
   return PreviewThemeData(
     materialLight: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      useMaterial3: true,
+    ),
+    materialDark: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       useMaterial3: true,
     ),
